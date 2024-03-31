d5d4f5998562f49a83ace44e4887c0d19996ecdc
”sunxin“ <sunxin@sheca.com>
2024-04-01 00:23:48 +0100

    add dropdown menu

--- src/main/resources/static/js/bookstore.js
+++ src/main/resources/static/js/bookstore.js
@@ -1,6 +1,16 @@
 $(document).ready(function (){
     console.log("document ready");
     findAll();
+    //browse-category-1
+    $('#browse-category-1').on('click', function (){
+        var width = $(this).outerWidth(); // 获取.browse-category的宽度，包括padding和border
+        $('.dropdown-menu').width(width); // 设置.dropdown-menu的宽度
+        //  var leftPos = $('#browse-category-1').offset().left;
+        // 将计算出的左边距赋值给.dropdown-menu
+        //  $('.dropdown-menu').css('left', leftPos + 'px');
+
+        $('.dropdown-menu').toggle(); // 切换显示状态
+    })
 })
 var rootURL = "http://localhost:8080/book";
 var findAll = function (){
