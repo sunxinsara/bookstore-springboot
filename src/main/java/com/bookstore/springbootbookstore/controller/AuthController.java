1c1a822957218d2db38f9361809ccb698df83a51
”sunxin“ <sunxin@sheca.com>
2024-04-06 17:09:19 +0100

    add security part

--- src/main/java/com/bookstore/springbootbookstore/controller/AuthController.java
+++ src/main/java/com/bookstore/springbootbookstore/controller/AuthController.java
@@ -1,4 +1,45 @@
 package com.bookstore.springbootbookstore.controller;
 
+import com.bookstore.springbootbookstore.dto.UserDto;
+import com.bookstore.springbootbookstore.service.UserService;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class AuthController {
+    private UserService userService;
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model){
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
 }
